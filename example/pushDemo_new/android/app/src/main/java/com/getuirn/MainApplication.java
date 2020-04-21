d80dcbd9922980311d34eccea3dc02e3b5eecd50
wanghaobo <wanghb@getui.com>
2020-04-21 17:35:47 +0800

    [feat]更新example

--- example/pushDemo_new/android/app/src/main/java/com/getuirn/MainApplication.java
+++ example/pushDemo_new/android/app/src/main/java/com/getuirn/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.getui.reactnativegetui.GetuiPackage;
+import com.getui.reactnativegetui.GetuiPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
